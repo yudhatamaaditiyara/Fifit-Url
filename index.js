e91a274e562948054343c139259df2dc3c9a51fd
Yudha Tama Aditiyara <yudhatamaaditiyara@gmail.com>
2019-11-21 02:28:36 +0700

    style: convert tab to whitespace

--- index.js
+++ index.js
@@ -30,117 +30,117 @@ const charsRegexp = /[\x09\x0A\x0C\x0D\x20\x22\x27\x3C\x3E\x5C\x5E\x60\x7B\x7C\x
  */
 class Url extends url.Url
 {
-	/**
-	 * @param {string} url
-	 * @returns {Url}
-	 */
-	parse(url){
-		if (typeof url == 'string' && url.charCodeAt(0) === 0x2f && !charsRegexp.test(url)) {
-			return this._parsePath(url);
-		}
-		return super.parse(url);
-	}
+  /**
+   * @param {string} url
+   * @returns {Url}
+   */
+  parse(url){
+    if (typeof url == 'string' && url.charCodeAt(0) === 0x2f && !charsRegexp.test(url)) {
+      return this._parsePath(url);
+    }
+    return super.parse(url);
+  }
 
-	/**
-	 * @returns {string}
-	 */
-	get scheme(){
-		if (this._scheme == null) {
-			this._scheme = this.protocol ? this.protocol.replace(/\:$/, '') : '';
-		}
-		return this._scheme;
-	}
+  /**
+   * @returns {string}
+   */
+  get scheme(){
+    if (this._scheme == null) {
+      this._scheme = this.protocol ? this.protocol.replace(/\:$/, '') : '';
+    }
+    return this._scheme;
+  }
 
-	/**
-	 * @returns {string}
-	 */
-	get origin(){
-		if (this._origin == null) {
-			this._origin = this.protocol ? `${this.protocol}//${this.host}` : '';
-		}
-		return this._origin;
-	}
-	
-	/**
-	 * @returns {string}
-	 */
-	get username(){
-		if (this._username == null) {
-			this._parseAuth(this.auth);
-		}
-		return this._username;
-	}
-	
-	/**
-	 * @returns {string}
-	 */
-	get password(){
-		if (this._password == null) {
-			this._parseAuth(this.auth);
-		}
-		return this._password;
-	}
+  /**
+   * @returns {string}
+   */
+  get origin(){
+    if (this._origin == null) {
+      this._origin = this.protocol ? `${this.protocol}//${this.host}` : '';
+    }
+    return this._origin;
+  }
+  
+  /**
+   * @returns {string}
+   */
+  get username(){
+    if (this._username == null) {
+      this._parseAuth(this.auth);
+    }
+    return this._username;
+  }
+  
+  /**
+   * @returns {string}
+   */
+  get password(){
+    if (this._password == null) {
+      this._parseAuth(this.auth);
+    }
+    return this._password;
+  }
 
-	/**
-	 * @returns {URLSearchParams}
-	 */
-	get searchParams(){
-		if (this._searchParams == null) {
-			this._searchParams = new url.URLSearchParams(this.queryParams);
-		}
-		return this._searchParams;
-	}
+  /**
+   * @returns {URLSearchParams}
+   */
+  get searchParams(){
+    if (this._searchParams == null) {
+      this._searchParams = new url.URLSearchParams(this.queryParams);
+    }
+    return this._searchParams;
+  }
 
-	/**
-	 * @returns {Object}
-	 */
-	get queryParams(){
-		if (this._queryParams == null) {
-			this._queryParams = typeof this.query == 'string' ? querystring.parse(this.query) : Object.create(this.query);
-		}
-		return this._queryParams;
-	}
+  /**
+   * @returns {Object}
+   */
+  get queryParams(){
+    if (this._queryParams == null) {
+      this._queryParams = typeof this.query == 'string' ? querystring.parse(this.query) : Object.create(this.query);
+    }
+    return this._queryParams;
+  }
 
-	/**
-	 * @returns {string}
-	 */
-	get fragment(){
-		if (this._fragment == null) {
-			this._fragment = this.hash ? this.hash.replace(/^#/, '') : '';
-		}
-		return this._fragment;
-	}
-	
-	/**
-	 * @param {string|null} auth
-	 * @returns {Url}
-	 */
-	_parseAuth(auth){
-		if (auth) {
-			let array = auth.split(':', 2);
-			this._username = array[0];
-			this._password = array[1] || '';
-		} else {
-			this._username = '';
-			this._password = '';
-		}
-		return this;
-	}
+  /**
+   * @returns {string}
+   */
+  get fragment(){
+    if (this._fragment == null) {
+      this._fragment = this.hash ? this.hash.replace(/^#/, '') : '';
+    }
+    return this._fragment;
+  }
+  
+  /**
+   * @param {string|null} auth
+   * @returns {Url}
+   */
+  _parseAuth(auth){
+    if (auth) {
+      let array = auth.split(':', 2);
+      this._username = array[0];
+      this._password = array[1] || '';
+    } else {
+      this._username = '';
+      this._password = '';
+    }
+    return this;
+  }
 
-	/**
-	 * @param {string} path
-	 * @returns {Url}
-	 */
-	_parsePath(path){
-		let match = path.match(parseRegexp);
-		this.href = match[0];
-		this.path = match[1];
-		this.pathname = match[2];
-		this.search = match[3] || this.search;
-		this.query = match[4] || this.query;
-		this.hash = match[5] || this.hash;
-		return this;
-	}
+  /**
+   * @param {string} path
+   * @returns {Url}
+   */
+  _parsePath(path){
+    let match = path.match(parseRegexp);
+    this.href = match[0];
+    this.path = match[1];
+    this.pathname = match[2];
+    this.search = match[3] || this.search;
+    this.query = match[4] || this.query;
+    this.hash = match[5] || this.hash;
+    return this;
+  }
 }
 
 /**
